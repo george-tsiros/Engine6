9ae590700181d13f818b3019472f2878f3531ff7
george-tsiros <tsiros@gmail.com>
2021-12-12 13:23:43 +0200

    these shouldn't be there

--- CsFwExample/Assertions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CsFwExample;
-
-using System.Diagnostics;
-
-static class Assertions {
-    private static void AssertStopwatchFrequencyRepresentableAsDouble () {
-        Debug.Assert(Stopwatch.Frequency.ToString() == ((double)Stopwatch.Frequency).ToString());
-    }
-}
--- CsFwExample/NativeMethods.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CsFwExample;
-
-using System.Runtime.InteropServices;
-
-internal static class NativeMethods {
-    [DllImport("user32.dll", CallingConvention = CallingConvention.Winapi)]
-    unsafe internal static extern int RegisterRawInputDevices (RawInputDevice* device, uint count, uint size);
-}
