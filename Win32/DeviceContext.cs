549e024154ef842edae1bae476f7ddaff5a3d228
george-tsiros <tsiros@gmail.com>
2022-08-20 12:23:38 +0300

    made shader programs into instance classes. created separate DeviceContext:SafeHandle class for DCs

---
+++ Win32/DeviceContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Win32;
+
+using System.Runtime.InteropServices;
+using System;
+
+public class DeviceContext:SafeHandle {
+    private const string dll = "user32.dll";
+    [DllImport(dll, CallingConvention = CallingConvention.Winapi, SetLastError = true)]
+    private static extern IntPtr GetDC (IntPtr windowHandle);
+
+    [DllImport(dll, CallingConvention = CallingConvention.Winapi)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ReleaseDC (IntPtr hwnd, IntPtr dc);
+
+    readonly IntPtr WindowHandle;
+    public DeviceContext (IntPtr hwnd): base(GetDC(hwnd), true) {
+        WindowHandle = hwnd;
+    }
+    public override bool IsInvalid => IntPtr.Zero == handle;
+
+    protected override bool ReleaseHandle () => ReleaseDC(WindowHandle, handle);
+}
