1c869abdd79a3d2c161d4bdb27eb689ca77fe4f1
George Tsiros <gtsiros@ajpsoft.gr>
2022-09-12 17:37:14 +0300

    button event args added

---
+++ Win32/ButtonEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Win32;
+
+using System;
+
+public class ButtonEventArgs:EventArgs {
+    public readonly MouseButton Button;
+    public readonly PointShort Location;
+    public ButtonEventArgs (MouseButton button, PointShort location) {
+        Button = button;
+        Location = location;
+    }
+}
